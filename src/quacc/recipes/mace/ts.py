7414f1e70fa8fe91f0c64ff75d7b25893ecea305
Anup Kumar <anukumar94663@gmail.com>
2024-07-05 02:14:19 -0700

    Removed the call to add hessian and standard deviation in irc for mace.

--- src/quacc/recipes/mace/ts.py
+++ src/quacc/recipes/mace/ts.py
@@ -190,10 +190,8 @@ def irc_job(
     # Run IRC
     with change_settings({"CHECK_CONVERGENCE": False}):
         dyn = Runner(atoms, calc).run_opt(**opt_flags)
-        opt_irc_summary = _add_stdev_and_hess(
-            summarize_opt_run(
-                dyn, additional_fields={"name": f"mace_off IRC: {direction}"}
-            )
+        opt_irc_summary = summarize_opt_run(
+            dyn, additional_fields={"name": f"mace_off IRC: {direction}"}
         )
 
     # Run frequency job
